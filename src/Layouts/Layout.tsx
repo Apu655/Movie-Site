fdb38a4bbff7e22b1de423577d591dc384b8685b
unknown <apu.islam@g.bracu.ac.bd>
2023-03-15 18:34:47 +0600

    Layout Tweak"

---
+++ src/Layouts/Layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Navbar/Navbar'
+import Header from './Header/Header'
+
+type Props = {}
+
+const Layout = (props: Props) => {
+  return (
+    <>
+    <Navbar/>
+    <Header/>
+    </>
+  )
+}
+
+export default Layout
---
+++ src/Layouts/Navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import requests from "../../utils/requests";
+import { useRouter } from "next/router";
+const Navbar = () => {
+  const router = useRouter();
+
+  return (
+    <nav className="relative">
+      <div className="flex px-10 sm:px-20 space-x-10 sm:space-x-20 text-xl whitespace-nowrap overflow-x-scroll scrollbar-hide overflow-y-hidden">
+        {Object.entries(requests).map(([key, { title, url }]: any) => (
+          <h2
+            key={key}
+            className="hover:scale-125 hover:text-white active:text-red-500 cursor-pointer transition duration-100 transform"
+            onClick={() => {
+              router.push(`/?genre=${key}`);
+            }}
+          >
+            {title}
+          </h2>
+        ))}
+      </div>
+      <div className="absolute top-0 right-0 h-10 bg-gradient-to-l from-[#030e13] lg:w-[0%] w-[7%]"></div>
+    </nav>
+  );
+};
+
+export default Navbar;
