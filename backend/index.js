08fec80241587f0dd856dc8a4ffcffa7ba9154ae
Mazharul Islam Rakib <mazharul.islam.rakib@g.bracu.ac.bd>
2023-03-30 00:04:44 +0600

    Accept Changes

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.send("Hello Express");
+})
+
+app.listen(5000, () => {
+    console.log("Server is running on port 5000");
+})
