4b2133012e57a88a284a2b3b6f7ee71f45ace5fc
Mazharul Islam Rakib <mazharul.islam.rakib@g.bracu.ac.bd>
2023-04-01 22:43:33 +0600

    Accept

---
+++ backend/database.js
@@ -0,0 +1,19 @@
+const{
+    createPool
+} = require('mysql');
+
+const pool = createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "movie site",
+    connectionLimit: 10
+})
+
+pool.query(`select * from movies`, (err, result, fields)=>{
+    if (err) {
+        return console.log(err);
+        
+    }
+    return console.log(result)
+})
