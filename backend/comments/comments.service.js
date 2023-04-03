5438bf6ac9091f1962f5a933efaabaf536b19c08
unknown <apu.islam@g.bracu.ac.bd>
2023-04-03 23:02:43 +0600

    Added backend Login and comment system

---
+++ backend/comments/comments.service.js
@@ -0,0 +1,35 @@
+const pool = require("../database")
+require("dotenv").config()
+
+module.exports = {
+    create:(data,callback)=>{
+        pool.query(`INSERT INTO Comments_Table VALUES(?,?,?,?,?)`,[data.id,data.comment,data.movie_id,data.likes,data.author],
+        (error,results,fields)=>{
+            if (error){
+                return callback(error)
+            }
+            return callback(null,results)
+        }
+        )
+    },
+    getComments:(movie_id,callback)=>{
+        pool.query(`SELECT * FROM comments_table WHERE movie_id=?`,[parseInt(movie_id)],
+        (error,results,fields)=>{
+
+            if (error){
+                return callback(error)
+            }
+            return callback(null,results)
+        })
+    },
+    deleteCommentById:(id,callback)=>{
+        pool.query(`DELETE FROM comments_table WHERE id = ? `,[id],
+        (error,results,fields)=>{
+            if (error){
+                return callback(error)
+            }
+            return callback(null,error)
+        }
+        )
+    }
+}
---
+++ backend/database.js
@@ -0,0 +1,30 @@
+const {createPool} = require('mysql')
+require('dotenv').config();
+
+// Tried with postgressSQL
+// const pool = createPool({
+//     host:process.env.DB_HOST,
+//     user:process.env.DB_USER,
+//     password:process.env.DB_PASSWORD,
+//     database:process.env.MYSQL_DB,
+//     connectionsLimit:10
+// });
+
+const pool = createPool({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"movie_stream_site",
+    connectionsLimit:10,
+    
+})
+
+
+// pool.query(`select * from users where`,(err,result,fields)=>{
+//     if(err){
+//         return console.log(err)
+//     }
+//     return console.log(result)
+// })
+
+module.exports = pool;
