79ae329c0ee866e7191cf07924dac0bf367fe397
apu655 <apu.islam@g.bracu.ac.bd>
2023-04-26 18:21:07 +0600

    Tweak

--- src/app/movie/[name]/page.tsx
+++ src/app/movie/[name]/page.tsx
@@ -4,18 +4,18 @@ import Movies from "@/components/Movies";
 import { SyntheticEvent, useEffect, useState } from "react";
 import axios from "axios";
 import { useAppSelector } from "@/Redux/hooks";
+import CommentBox from "@/components/CommentBox";
 
 const Details = ({ params }: any) => {
-  const auth = useAppSelector(state=>state.auth)
+  const auth = useAppSelector((state) => state.auth);
   const [results, setResults] = useState();
   const [trailer, setTrailer] = useState();
-  const [comments, setComments] = useState<any>([]);
-  const [update,setUpdate] = useState({
-    isEdit:false,
-    comment:"",
-    id:""
 
-  })
+  const [update, setUpdate] = useState({
+    isEdit: false,
+    comment: "",
+    id: "",
+  });
 
   const movie_id = params.name;
   const getData = async () => {
@@ -34,93 +34,17 @@ const Details = ({ params }: any) => {
       }
     });
   };
-  const getComments = async () => {
-    const { data } = await axios.get(
-      `http://localhost:5000/comment/get?movie_id=${movie_id}`
-    );
-    setComments(data.data);
-  };
+
   useEffect(() => {
     getData();
-    getComments();
   }, []);
-
-  const handleDelete = async (id:string)=>{
-    const {data} = await axios.delete(`http://localhost:5000/comment/delete/${id}`)
-    getComments()
-    
-  }
-  const [formData, setFormData] = useState("");
-  useEffect(()=>{
-    setFormData(
-      {
-      movie_id:movie_id,
-      likes:0,
-      author:auth.user.name,
-      comment:""
-    }
-    )
-  },[])
-
-  const handleChange = (e:any)=>{
-    const {value,name} = e.target
-    console.log(formData)
-    setFormData((prev:any)=>{
-      return {...prev,[name]:value}
-    })
-    
-
-  }
-  const handleSubmit = async (e: SyntheticEvent) => {
-    e.preventDefault();
-    const {data} = await axios.post(`http://localhost:5000/comment/post`,formData)
-    console.log("");
-    setFormData((prev)=>{return {...prev,['comment']:""}})
-    getComments()
-  };
 
   return (
     <>
       <div>
-        <div>
-          {trailer && <Movies result={results} trailer={trailer}></Movies>}
-          <div className="lg:max-w-6xl mx-auto">
-            <form onSubmit={handleSubmit} className="relative">
-              <input
-                className="w-full rounded py-1 px-2 text-black"
-                placeholder="Write a comment ..."
-                onChange={handleChange}
-                value={formData.comment}
-                name="comment"
-              />
-              <button
-                type="submit"
-                className="absolute px-2 py-1 bg-slate-500 right-0"
-              >
-                Submit
-              </button>
-            </form>
-            <div className="flex flex-col mt-4 space-y-4  text-white">
-              {comments.length >= 0 &&
-                comments.map((data: any) => (
-                  <div className="text-black flex space-x-4">
-                    <p className="font-bold px-3 bg-gray-100 text-center items-center flex text-sm rounded-full">
-                      Picture
-                    </p>
-                    <div className="flex flex-col py-3 px-4 space-y-2 rounded-md bg-gray-100">
-                      <p className="text-xs font-bold">{data.author}</p>
-                      <p className="font-semibold">
-                        {data.comment}
-                      </p>
-                      <div className="flex space-x-5">
-                        <p className="text-xs p-2 hover:bg-gray-300 rounded text-gray-500 cursor-pointer">Edit</p>
-                        <p onClick={()=>{handleDelete(data.id)}} className="text-xs p-2 hover:bg-red-300 rounded text-red-500 cursor-pointer">Delete</p>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-            </div>
-          </div>
+        {trailer && <Movies result={results} trailer={trailer}></Movies>}
+        <div className="lg:max-w-6xl mx-auto">
+          <CommentBox movie_id={movie_id} />
         </div>
       </div>
     </>
---
+++ src/components/CommentBox.tsx
@@ -0,0 +1,103 @@
+import { useAppSelector } from "@/Redux/hooks";
+import { axios } from "@/config";
+import React, { useEffect, useState } from "react";
+
+type Props = {};
+
+const CommentBox = ({ movie_id }) => {
+  const [comments, setComments] = useState<any>([]);
+  const auth = useAppSelector((state) => state.auth);
+  const getComments = async () => {
+    const { data } = await axios.get(
+      `http://localhost:5000/comment/get?movie_id=${movie_id}`
+    );
+    setComments(data.data);
+  };
+  useEffect(() => {
+    getComments();
+  }, []);
+
+  const handleDelete = async (id: string) => {
+    const { data } = await axios.delete(
+      `http://localhost:5000/comment/delete/${id}`
+    );
+    getComments();
+  };
+  const [formData, setFormData] = useState("");
+  useEffect(() => {
+    setFormData({
+      movie_id: movie_id,
+      likes: 0,
+      author: auth.user.name,
+      comment: "",
+    });
+  }, []);
+
+  const handleChange = (e: any) => {
+    const { value, name } = e.target;
+    console.log(formData);
+    setFormData((prev: any) => {
+      return { ...prev, [name]: value };
+    });
+  };
+  const handleSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    const { data } = await axios.post(
+      `http://localhost:5000/comment/post`,
+      formData
+    );
+    console.log("");
+    setFormData((prev: any) => {
+      return { ...prev, ["comment"]: "" };
+    });
+    getComments();
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="relative">
+        <input
+          className="w-full rounded py-1 px-2 text-black"
+          placeholder="Write a comment ..."
+          onChange={handleChange}
+          value={formData.comment}
+          name="comment"
+        />
+        <button
+          type="submit"
+          className="absolute px-2 py-1 bg-slate-500 right-0"
+        >
+          Submit
+        </button>
+      </form>
+      <div className="flex flex-col mt-4 space-y-4  text-white">
+        {comments.length >= 0 &&
+          comments.map((data: any) => (
+            <div className="text-black flex space-x-4">
+              <p className="font-bold px-3 bg-gray-100 text-center items-center flex text-sm rounded-full">
+                Picture
+              </p>
+              <div className="flex flex-col py-3 px-4 space-y-2 rounded-md bg-gray-100">
+                <p className="text-xs font-bold">{data.author}</p>
+                <p className="font-semibold">{data.comment}</p>
+                <div className="flex space-x-5">
+                  <p className="text-xs p-2 hover:bg-gray-300 rounded text-gray-500 cursor-pointer">
+                    Edit
+                  </p>
+                  <p
+                    onClick={() => {
+                      handleDelete(data.id);
+                    }}
+                    className="text-xs p-2 hover:bg-red-300 rounded text-red-500 cursor-pointer"
+                  >
+                    Delete
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default CommentBox;
